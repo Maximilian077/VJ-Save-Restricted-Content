70305c5a027d1c9a117e8209d3b83da4d1381a66
Maximilian077 <milanrana0777@gmail.com>
2024-08-17 12:12:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,11 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "28523160"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "4c3c1bc08310747eb8d52f9aadf82bcd")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Stranger_0777:IE7a7kW1bEoEuZNv@cluster0.tsbxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+")
